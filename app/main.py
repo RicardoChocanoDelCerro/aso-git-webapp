d16afe7d3ed4f173be8e0751255128cbdb5797d8
Ricardo Chocano <ricardo.ch.92.rc@gmail.com>
2023-12-04 17:20:31 +0000

    mis cambios

--- app/main.py
+++ app/main.py
@@ -5,17 +5,23 @@ import redis
 
 APP = flask.Flask(__name__)
 
+is is_redis_available:
+    redis_client = redis.Redis(host=REDISHOST, port=REDISPORT, decode_responses=True)
+
+
 @APP.route('/')
 def index():
+    global redis_client
 
-    r = redis.Redis(host=REDISHOST, port=REDISPORT, decode_responses=True)
-
-    num_visitas = r.incr("contrador_visitas")
+    if is_redis_available:
+        num_visitas = r.incr("contrador_visitas")
+    else
+        num_visitas = None
 
     hostname = socket.gethostname()
     userinfo = {
         'username': os.environ['CLIENT_NAME']
-    }
+    
     return flask.render_template(
             'index.html', 
             user=userinfo, 
@@ -26,5 +32,7 @@ if __name__ == '__main__':
     PORT=os.environ['PORT']
     REDISPORT=os.environ['REDISPORT']
     REDISHOST=os.environ['REDISHOST']
-    APP.debug = True
+    IS_REDIS_AVAILABLE=os.environ['IS_REDIS_AVAILABLE']
+    IS_REDIS_AVAILABLE=True if IS_REDIS_AVAILABLE.upper()="TRUE" else False
+    APP.debug = os.environ.get("DEBUG", True).upper() == "TRUE":
     APP.run(host='0.0.0.0', port=PORT)
